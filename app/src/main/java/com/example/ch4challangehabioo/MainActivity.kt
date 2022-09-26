50382d163c8ea3fc1122773bb83a4374989360b7
habiofatwasidiq <habiofatwasidiq@gmail.com>
2022-09-26 21:56:43 +0700

    finishing intent

--- app/src/main/java/com/example/ch4challangehabioo/MainActivity.kt
+++ app/src/main/java/com/example/ch4challangehabioo/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.ch4challangehabioo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ch4challangehabioo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding:ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        binding.ivRefresh.setOnClickListener{
+            val intent = intent
+            finish()
+            startActivity(intent)
+        }
     }
 }
